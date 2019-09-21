fd4e959d18d67cbbb37d0ce6aa814d193132842c
kannoy <ec2-user@ip-172-31-37-151.us-east-2.compute.internal>
2019-09-21 12:40:17 +0000

    php10-4

---
+++ resources/views/admin/news/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UFT-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>Myニュース作成画面</h1>
+        <h2>課題10-4</h2>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UFT-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MyProfile</title>
+    </head>
+    <body>
+        <h1>Myプロフィール変更画面</h1>
+        <h2>課題10-4</h2>
+    </body>
+</html>
