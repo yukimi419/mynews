fe277eb1f534a9bab08c628efef989ed1dd3e27a
kannoy <ec2-user@ip-172-31-37-151.us-east-2.compute.internal>
2019-09-21 03:26:22 +0000

    php09-4

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function(){
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
