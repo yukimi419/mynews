6c4d6115ac12eb136c7b37748449ac0ad3b0effb
kannoy <ec2-user@ip-172-31-37-151.us-east-2.compute.internal>
2019-09-22 10:01:10 +0000

    php11-8

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UFT-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>MyProfile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+@section('title', 'プロフィールの新規作成')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,15 @@ Route::group(['prefix' => 'admin'], function(){
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
