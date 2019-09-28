92134245f01851994da63545c2129bba9431d920
kannoy <ec2-user@ip-172-31-37-151.us-east-2.compute.internal>
2019-09-28 10:18:09 +0000

    php14

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\News;
 
 class NewsController extends Controller
 {
@@ -12,7 +13,25 @@ class NewsController extends Controller
     }
     
     public function create(Request $request){
-    return redirect('admin/news/create');
+        $this->validate($request, News::$rules);
+        
+        $news =new News;
+        $form = $request->all();
+        
+        if(isset($form['image'])){
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        }else{
+            $news->image_path =  null;
+        }
+        
+        unset($form['_token']);
+        unset($form['image']);
+        
+        $news->fill($form);
+        $news->save();
+        
+        return redirect('admin/news/create');
     }
 }
 
